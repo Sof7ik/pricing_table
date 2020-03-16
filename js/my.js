8471a2ce6fb34bd2f6cec819aa12d875ded423a2
Leonid <Bychkov>
2020-03-16 16:26:32 +0300

    ajax

---
+++ js/my.js
@@ -0,0 +1,99 @@
+const bodyElement = document.body;
+let name, tel;
+
+
+
+const closeModal = () => {
+    if(document.querySelector('div.modal'))
+    {
+        document.querySelector('div.modal').remove();  
+    }
+}
+
+document.body.addEventListener('click', (event) => {
+    if (event.target.classList.contains('button-get-started'))
+    {
+        createModal();
+    }
+
+    if (event.target === document.querySelector('div.modal'))
+    {
+
+        closeModal();
+    }
+})
+
+const sendFeedback = (event) => {
+    event.preventDefault();
+    
+    name = document.getElementById('name').value;
+    console.log('name: ', name);
+    tel = document.getElementById('tel').value;
+    console.log('tel: ', tel);
+
+    if(name.trim() !== '' & tel.trim() !== ''){
+        let formData = new FormData();
+        formData.append('name', name);
+        formData.append('tel', tel);
+
+        fetch('../phpScripts/send.php', 
+        {
+            method: 'POST',
+            body: `name=${name}&tel=${tel}`
+        })
+        .then((res) =>
+        {
+            return res.text();
+        })
+        .then((res) =>
+        {
+            console.log(res);
+        });
+    }
+    else {
+        console.log('Поля заполни дебил!');
+    }
+    // const req = new XMLHttpRequest();
+    // const url = '../phpScripts/send.php';
+    // const params = `name=${name}&tel=${tel}`;
+    // req.open(POST, url, true);
+
+    // req.addEventListener('readystatechange', () => {
+    //     if (req.readyState === 200)
+    //     {
+    //         console.log(200);
+    //     }
+
+    //     if (req.readyState === 300)
+    //     {
+    //         console.log(300);
+    //     }
+
+    //     if (req.readyState === 400)
+    //     {
+    //         console.log(400);
+    //         console.log('Запрос отправлен');
+    //     }
+    // })
+
+    // req.send(params);
+}
+
+const createModal = () => {
+    const modal = document.createElement('div');
+    modal.classList.add('modal');
+    modal.innerHTML = `<div class="innerModal">
+        <h1 class="modalTitle">Callback feedback</h1>
+        <span class="modalDesc">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi corporis quo, fuga reprehenderit quisquam asperiores recusandae ducimus dicta vel praesentium veritatis magni placeat eligendi provident consequatur iste. Enim, quo rem?</span>
+        <form action="" method="POST">
+            <input type="text" name="name" id="name" placeholder="Enter your name here">
+            <input type="tel" name="tel" id="tel" placeholder="Enter your tel number here">
+
+            <input type="submit" value="Get callback!">
+        </form>
+    </div>`;
+    bodyElement.insertAdjacentElement('afterbegin', modal);
+    modal.style.minHeight = '100vh';
+    document.querySelector('input[type="submit"]').addEventListener('click', sendFeedback);
+}
+
--- phpScripts/send.php
+++ phpScripts/send.php
@@ -1,4 +1,4 @@
-<?
+<?php
 $host = "localhost";
 $user = "root";
 $password = "";
@@ -7,13 +7,17 @@ $db_name = "pricing";
 $link = new mysqli($host, $user, $password, $db_name);
 
 // Проверка на подключение к базе
-if ($link->connet_error)
+if ($link->connect_error)
 {
     die ("Connection failed: " . $link->connect_error);
 }
 
 $name = $_POST['name'];
-$tel = $_POST['phoneNumber'];
+$tel = $_POST['tel'];
+
+echo $name . ' -  name' . "\n";
+echo $tel . ' -  tel' . "\n1213213213";
+
 
 $query = mysqli_query($link, "INSERT INTO `callbacks` (`id`,`NaS`,`tel`) VALUES (NULL, '$name', '$tel')")
 ?>
